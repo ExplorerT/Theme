89ab8f56cb5c69b86aa22eb581525a9613251f37
Tolu Agbaje <tolu@purposesocial.com>
2017-06-12 22:19:38 -0700

    Add homepage template

---
+++ Homepage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package LAVA
+ */
+
+get_header(); ?>
+
+		
+
+			<div class="video-container"> 
+				<iframe width="560" height="315" src="http://www.youtube.com/embed/S95J5BowMmk" frameborder="0" allowfullscreen></iframe> 
+			</div>
+
+
+<?php
+get_sidebar();
+get_footer();
